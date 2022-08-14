84cbc19f23ddb492e25db5d83be08afb08f41fd5
3rix4 <3rix40@gmail.com>
2022-08-14 17:04:42 -0500

    new section and photos

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import logo from './logo1.svg';
 import logoText from './logoText2.svg';
 import karenAndDog from './photos/karenAndDog.jpg';
+import shield from './shield.svg';
+import phone from './phone.svg';
 import './App.css';
 import { Link, Path, To } from 'react-router-dom';
 import { NumberLiteralType } from 'typescript';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 function Header(){
   return (
@@ -37,7 +40,7 @@ function ButtonLink({page, text}: {page: string, text: string}){
 
 function HereToHelp(){
   return (
-    <div className="HereToHelp">
+    <div className="HereToHelp section">
       <div className='col' id='helpColLeft'>
         <h1 className='sectionTitle'>We are here to help</h1>
         <p className='sectionText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
@@ -46,7 +49,7 @@ magna aliqua. Ut enim ad minim veniam, quis nostrud
 exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in
 voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-        <ButtonLink page='process' text='Our Process'/>
+        <ButtonLink page='/process' text='Our Process'/>
       </div>
       <div className='col' id='helpColRight'>
         <img src={karenAndDog} className="karenAndDog" alt="Dr. Karen"></img>
@@ -57,9 +60,38 @@ voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
 
 function ProfessionalService(){
   return (
-    <div className='ProfessionalService'>
-      <div className='col'>
+    <div className='ProfessionalService section'>
+      <div className='col' id='profColLeft'>
+        <img src={shield} className="shield" alt="shield"></img>
+      </div>
+      <div className='col'  id='profColRight'>
         <h1 className='sectionTitle'>Professional Service</h1>
+        <p className='sectionText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+sed do eiusmod tempor incididunt ut labore et dolore
+magna aliqua.</p>
+        <ButtonLink page='/about' text='About Us'/>
+      </div>
+    </div>
+  );
+}
+
+function OurDoors(){
+  return (
+    <div className='OurDoors section'>
+      <div className='col' id="doorsLeftCol">
+        <h1 className='sectionTitle'>Our doors are always open</h1>
+        <p className='sectionText'>I regret to say that I do have limited availability due to working
+in other clinics and raising a family. Please contact me so we
+can work together to find a time that we can help. My fee is
+$220 and includes a clay paw print. I accept cash, checks and
+all major credit cards.</p>
+        <ButtonLink page='/contact' text='Contact Us'/>
+      </div>
+      <div className='col' id="doorsRightCol">
+        <a id="phoneLink" href='tel:5072621896'>
+          <img src={phone} className="phone" alt="phone"></img>
+          <p id="phoneNumber">507-262-1896</p>
+        </a>
       </div>
     </div>
   );
@@ -68,17 +100,20 @@ function ProfessionalService(){
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <nav className="HomeRouter">
-        <div className='routerLinks'>
-          <PageLink page='/process' text='Our Process'/>
-          <PageLink page='/testimonials' text='Testimonials'/>
-          <PageLink page='/contact' text='Contact Us'/>
-          <PageLink page='/about' text='About Us'/>
-        </div>
-      </nav>
-      <HereToHelp/>
-      <ProfessionalService/>
+      <Scrollbars>
+        <Header/>
+        <nav className="HomeRouter">
+          <div className='routerLinks'>
+            <PageLink page='/process' text='Our Process'/>
+            <PageLink page='/testimonials' text='Testimonials'/>
+            <PageLink page='/contact' text='Contact Us'/>
+            <PageLink page='/about' text='About Us'/>
+          </div>
+        </nav>
+        <HereToHelp/>
+        <ProfessionalService/>
+        <OurDoors/>
+      </Scrollbars>
     </div>
   );
 }
